02cde464c07d7fe66ef3f28f6fb82154a91af23b
Sean Purcell <s3purcell@gmail.com>
2015-07-20 22:58:12 -0700

    serve a polymer element, basic post endpoint for graphql queries

--- src/app.js
+++ src/app.js
@@ -1,20 +1,51 @@
-var mw = require("mediaWatcher");
-var tracks = require("./tracks");
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
-var KNOWN_PATHS ={
-      "7076436f-6e65-1063-8074-4ce6766160b7" : "1$268435466",   //Linkstation
-}
+import { TrackSchema } from './schema.js';
+import { graphql } from 'graphql';
 
-mw.on("serverAdded",function(ev){
-  if(KNOWN_PATHS[ev.uuid]){
-    console.log("get tracks")
-    mw.getTracks(ev.uuid,KNOWN_PATHS[ev.uuid],function(err,data){
-      console.log("insert tracks",err)
-      if(!err){
-        tracks.insertTracks(data)
-        .then( () => console.log("tracks inserted"))
-        .catch( (e) => console.log(e))
-      }
-    })
-  }
+
+app.use(express.static('app/public'));
+app.use(express.static('app'));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.sendFile('index.html',{
+    root: __dirname + '/../app/public/'
+  });
+});
+
+app.post("/api/graphql", function(req,res){
+  graphql(TrackSchema,req.body.query)
+  .then( (data) => res.send(data))
+  .catch( (err) => res.send(err) )
 })
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
+// var mw = require("mediaWatcher");
+// var tracks = require("./tracks");
+//
+// var KNOWN_PATHS ={
+//       "7076436f-6e65-1063-8074-4ce6766160b7" : "1$268435466",   //Linkstation
+// }
+//
+// mw.on("serverAdded",function(ev){
+//   if(KNOWN_PATHS[ev.uuid]){
+//     console.log("get tracks")
+//     mw.getTracks(ev.uuid,KNOWN_PATHS[ev.uuid],function(err,data){
+//       console.log("insert tracks",err)
+//       if(!err){
+//         tracks.insertTracks(data)
+//         .then( () => console.log("tracks inserted"))
+//         .catch( (e) => console.log(e))
+//       }
+//     })
+//   }
+// })
